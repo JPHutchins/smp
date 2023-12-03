852bcf7658895b64f24386ff7fef7b19b14b3c63
J.P. Hutchins <jphutchins@gmail.com>
2023-12-02 16:51:58 -0800

    feat: add OS Management errors

--- smp/os_management.py
+++ smp/os_management.py
@@ -1,7 +1,9 @@
 """The Simple Management Protocol (SMP) OS Management group."""
 
 
-from smp import header, message
+from enum import IntEnum, auto, unique
+
+from smp import error, header, message
 
 
 class EchoWriteRequest(message.WriteRequest):
@@ -26,3 +28,23 @@ class ResetWriteRequest(message.WriteRequest):
 class ResetWriteResponse(message.WriteResponse):
     _GROUP_ID = header.GroupId.OS_MANAGEMENT
     _COMMAND_ID = header.CommandId.OSManagement.RESET
+
+
+@unique
+class OS_MGMT_RET_RC(IntEnum):
+    OK = 0
+    """No error, this is implied if there is no ret value in the response."""
+
+    UNKNOWN = auto()
+    """Unknown error occurred."""
+
+    INVALID_FORMAT = auto()
+    """The provided format value is not valid."""
+
+
+class OSManagementErrorV0(error.ErrorV0[OS_MGMT_RET_RC]):
+    _GROUP_ID = header.GroupId.OS_MANAGEMENT
+
+
+class OSManagementErrorV1(error.ErrorV1[OS_MGMT_RET_RC]):
+    _GROUP_ID = header.GroupId.OS_MANAGEMENT
